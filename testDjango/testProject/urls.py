0c2853e31b40d01eb4e32317f952d614399b31d7
Junki Lee <1@yousei.net>
2022-04-28 00:19:35 +0900

    회원가입까지 구현

--- testDjango/testProject/urls.py
+++ testDjango/testProject/urls.py
@@ -1,21 +1,12 @@
-"""testProject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url="/home/", permanent = True)), #  path에 공백을 넣으면 /home으로 리다이렉트되는 코드. :8000/입력도 :8000/home/ 페이지로 강제이동
+    path('user/', include('user.urls')),
 ]
